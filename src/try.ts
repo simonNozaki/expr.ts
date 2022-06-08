d1a8d09949f9c9fca4116d1459526e7b2af81bd9
snozaki <snozaki@netprotections.co.jp>
2022-06-09 08:25:08 +0900

    MOD: update try-catch o run successfully

--- src/try.ts
+++ src/try.ts
@@ -2,8 +2,8 @@
  * Expression closure definition.
  */
 type ExpressionState<T> = {
-  e: typeof Error,
-  f: (e?: Error) => T
+  e: Error,
+  f: (e: Error) => T
 }
 
 
@@ -13,33 +13,46 @@ type ExpressionState<T> = {
  * @param {...ExpressionState<T>[]} catches `catch` expressions
  * @return {T} t
  */
-export const Try = <T>(f: () => T, ...catches: ExpressionState<T>[]): T => {
+export const doOnTry = <T>(f: () => T, ...catches: ExpressionState<T>[]): T => {
   try {
     return f();
   } catch (e) {
-    for (let i = 0; i < catches.length; i++) {
-      const expr = catches[i];
-      if (e instanceof expr.e) {
-        return expr.f(e);
-      }
+    if (!(e instanceof Error)) {
+      throw e;
     }
-    throw e;
+
+    return catches
+        .find((c) => c.e.name === e.name)
+        .f(e);
   }
 };
 
 /**
  * `catch` expression. This function enclose target error type
  * and error handler.
+ * `doOnTry` expression can handle some `recover` functions like
+ * try-catch statement as we know, written below:
+ * ```
+ * try {
+ *   execute();
+ * } catch (IllegalArgumentException iae) {
+ *   handleIllegalArgumentException();
+ * } catch (FileNotFoundException fnf) {
+ *   handleFileNotFoundException();
+ * } catch (Exception e) {
+ *   handleException();
+ * }
+ * ```
  * @param {Error} e Error type infomation to handle.
  * @param {()} f Consumer to be executed
  * @return {T} Expression state of catch expression.
  */
-export const Catch = <T, E extends Error | Error>(
-  e: {new (): E}, f: (e?: E) => T,
+export const recover = <T, E extends Error>(
+  e: E, f: (e: E) => T,
 )
 : ExpressionState<T> => {
   return {
-    e: typeof e,
+    e: e,
     f: f,
   };
 };
