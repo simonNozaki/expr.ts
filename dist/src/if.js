b984ec00d3945d1807b1719e133352864a7f0f94
snozaki <snozaki@netprotections.co.jp>
2022-06-12 10:31:32 +0900

    MOD: update docs

--- dist/src/if.js
+++ dist/src/if.js
@@ -7,19 +7,22 @@ var If = function (condition, block) {
 exports.If = If;
 var Evaluator = (function () {
     function Evaluator(condition, block) {
-        this.condition = condition;
-        this.block = block;
+        this.condtionFunctions = [];
+        this.condtionFunctions.push([condition, block]);
     }
-    Evaluator.prototype.else = function (alternative) {
-        return this.evaluate(alternative);
+    Evaluator.prototype.elseIf = function (condition, block) {
+        this.condtionFunctions.push([condition, block]);
+        return this;
     };
-    Evaluator.prototype.evaluate = function (alternative) {
-        if (this.condition) {
-            return this.block();
+    Evaluator.prototype.else = function (alternative) {
+        this.condtionFunctions.push([true, alternative]);
+        for (var _i = 0, _a = this.condtionFunctions; _i < _a.length; _i++) {
+            var pair = _a[_i];
+            if (pair[0]) {
+                return pair[1]();
+            }
         }
-        else {
-            return alternative();
-        }
+        throw new Error('All conditions was not matched');
     };
     return Evaluator;
 }());
--- dist/test/if-tests.js
+++ dist/test/if-tests.js
@@ -3,6 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var src_1 = require("../src");
 describe('If tests', function () {
     var base = 'The quick brown fox jumps over the lazy dog';
+    var StatusFindService = (function () {
+        function StatusFindService() {
+        }
+        StatusFindService.prototype.execute = function () {
+            return '5';
+        };
+        return StatusFindService;
+    }());
     it('evaluate if expression', function () {
         var r = (0, src_1.If)(base.length > 30, function () { return 'over 30'; })
             .else(function () { return 'under 30'; });
@@ -12,6 +20,14 @@ describe('If tests', function () {
         var r = (0, src_1.If)(base.length > 50, function () { return 'over 50'; })
             .else(function () { return 'under 50'; });
         expect(r).toBe('under 50');
+    });
+    it('evaluate elseIf and else sequentially', function () {
+        var result = new StatusFindService().execute();
+        var res = (0, src_1.If)(result === '0', function () { return ({ message: 'success' }); })
+            .elseIf(result === '5', function () { return ({ message: 'not found' }); })
+            .else(function () { return ({ message: 'failure' }); });
+        console.log(res);
+        expect(res).toStrictEqual({ message: 'not found' });
     });
 });
 //# sourceMappingURL=if-tests.js.map
